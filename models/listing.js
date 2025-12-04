af039533d36ab08d91d77f52b47d14ad87421282
iamaabhishek <abhishekaydv01@gmail.com>
2025-12-04 10:28:19 +0530

    add init folder and Some changes

--- models/listing.js
+++ models/listing.js
@@ -2,34 +2,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const listingSchema = new Schema({
-    title:{
-        type:String,
-        require:true
-    },
-    description:{
+    title: {
         type: String,
-        
+        required: true
     },
-    image:{
-        type:String,
-        set: (v) => v === "" ? "https://images.unsplash.com/photo-1566073771259-6a8506099945?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
-         
+
+    description: {
+        type: String
     },
-    price:String,
-    location:String,
-    country:String
-    
-})
+
+    image: {
+        filename: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+
+    price: {
+        type: String
+    },
+
+    location: {
+        type: String
+    },
+
+    country: {
+        type: String
+    }
+});
 
 
-const Listing = mongoose.model("Listing",listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 
 module.exports = Listing;
-
-
-
-
-
-
-
-
